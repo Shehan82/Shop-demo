614d344df6e2d6fa5ae82f36c092e4e3d56a4567
shehan82 <shehansandeepa82@gmail.com>
2020-12-20 23:15:33 +0530

    add todo and display using useStates

--- App.js
+++ App.js
@@ -1,28 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView , TextInput, Button, TouchableOpacity} from 'react-native';
 import Todo from './Todo';
 
 export default function App() {
+
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(["sheahn"]);
+
+  const addToDo = ()=>{
+    setTodos([input, ...todos]);
+  }
+
+
   return (
-    <SafeAreaView  style={styles.container}>
+    <SafeAreaView  >
+      <View >
           <Text style={styles.txt}>Shehan</Text>
-          <Todo name={"shehan"}/>
+          {/* <Todo name={"shehan"}/> */}
+          <View>
+            <Text>Input your TODO here!</Text>
+            <TextInput
+             
+              style={styles.todoInput}
+              onChange={(e) => {setInput(e.target.value)}}
+            
+            />
+
+            
+
+            <Button 
+            title="Add TODO"
+            onPress={addToDo}
+            />
+          </View>
+
+          {todos.map(todo =>(
+            <Text >{todo}</Text>
+          ))}
+
+
+          
+
+      </View>
     </SafeAreaView>
     
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'purple',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize:50,
-  },
-
+  
   txt:{
-    fontSize:60,
+    fontSize:20,
+  },
+  todoInput:{
+    borderColor:'red',
+    borderWidth:1,
+    width:200,
+    alignItems:'center'
+    
   }
 });
