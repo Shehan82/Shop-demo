e1fb398b3ee37320ebcb783312032b464651ccfe
shehan82 <shehansandeepa82@gmail.com>
2020-12-21 23:13:27 +0530

    create alert

--- App.js
+++ App.js
@@ -1,43 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView ,TouchableWithoutFeedback ,TextInput, Button, TouchableOpacity, Switch, Image} from 'react-native';
-import Todo from './Todo';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Switch,
+  Image,
+} from "react-native";
+import Todo from "./Todo";
 
 export default function App() {
-
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState(["shehen sandeepa"]);
 
-  const addToDo = ()=>{
+  const addToDo = () => {
     setTodos([input, ...todos]);
-  }
+  };
 
   const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 
   return (
-    <SafeAreaView  style={styles.container} >
-     
-          <Text style={styles.txt}>Shehan</Text>
-          
-          <View>
-            <Text>Add your todo here bro!</Text>
-            <TextInput
-             
-              style={styles.todoInput}
-              onChangeText={text=>setInput(text)} 
-             
-            />
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.txt}>Shehan</Text>
 
-            <TouchableOpacity onPress={addToDo} style={styles.buttonStyle}>
-              <Text style={styles.buttonText}>Add Todo</Text>
-            </TouchableOpacity>
-          </View>
+      <View>
+        <Text>Add your todo here bro!</Text>
+        <TextInput
+          style={styles.todoInput}
+          onChangeText={(text) => setInput(text)}
+        />
 
-          {todos.map(todo =>(
-            <Text key={todo} style={{color:'red'}} >{todo}</Text>
-          ))}
+        <TouchableOpacity onPress={addToDo} style={styles.buttonStyle}>
+          <Text style={styles.buttonText}>Add Todo</Text>
+        </TouchableOpacity>
+      </View>
+
+      {todos.map((todo) => (
+        <Text key={todo} style={{ color: "red" }}>
+          {todo}
+        </Text>
+      ))}
 
       <Switch
         trackColor={{ false: "#767577", true: "#81b0ff" }}
@@ -46,57 +54,56 @@ export default function App() {
         onValueChange={toggleSwitch}
         value={isEnabled}
       />
-      <TouchableOpacity onPress={()=>{console.log("imagePressed!")}}>   
-     <Image
-     blurRadius={4}
-     style={styles.tinyLogo} 
-     source={require('./assets/sfd.png')}
-     />
-     </TouchableOpacity> 
-
+      <TouchableOpacity
+        onPress={() => {
+          alert("image pressed!");
+        }}
+      >
+        <Image
+          blurRadius={4}
+          style={styles.tinyLogo}
+          source={require("./assets/sfd.png")}
+        />
+      </TouchableOpacity>
     </SafeAreaView>
-    
   );
 }
 
 const styles = StyleSheet.create({
-  
-  txt:{
-    fontSize:20,
+  txt: {
+    fontSize: 20,
   },
-  todoInput:{
-    borderColor:'red',
-    borderWidth:1,
-    width:200,
-    alignItems:'center'
-    
+  todoInput: {
+    borderColor: "red",
+    borderWidth: 1,
+    width: 200,
+    alignItems: "center",
   },
   tinyLogo: {
     width: 200,
     height: 300,
   },
-  buttonStyle:{
-    backgroundColor:"blue",
-    width:100,
-    marginLeft:'auto',
-    marginRight:'auto',
-    alignItems:'center',
-    marginTop:20,
-    height:30,
-    borderRadius:10
+  buttonStyle: {
+    backgroundColor: "blue",
+    width: 100,
+    marginLeft: "auto",
+    marginRight: "auto",
+    alignItems: "center",
+    marginTop: 20,
+    height: 30,
+    borderRadius: 10,
   },
-  buttonText:{
-    color:'white',
-    marginTop:'auto',
-    marginBottom:'auto',
-    fontSize:16,
-    fontWeight:'800'
-
+  buttonText: {
+    color: "white",
+    marginTop: "auto",
+    marginBottom: "auto",
+    fontSize: 16,
+    fontWeight: "800",
   },
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center',
-    backgroundColor:"#fff"
-  }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
 });
