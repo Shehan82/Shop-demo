8c07b7eb00cca16ec7c5e7c9a245417f6475ea71
shehan82 <shehansandeepa82@gmail.com>
2020-12-21 22:23:18 +0530

    add the switch

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView , TextInput, Button, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView , TextInput, Button, TouchableOpacity, Switch} from 'react-native';
 import Todo from './Todo';
 
 export default function App() {
@@ -12,52 +12,40 @@ export default function App() {
     setTodos([input, ...todos]);
   }
 
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
 
   return (
     <SafeAreaView  >
       <View >
           <Text style={styles.txt}>Shehan</Text>
-          {/* <Todo name={"shehan"}/> */}
+          
           <View>
-            <Text>Add your todo here!</Text>
+            <Text>Add your todo here bro!</Text>
             <TextInput
              
               style={styles.todoInput}
               onChangeText={text=>setInput(text)} 
-            
+             
             />
 
-            <TouchableOpacity onPress={addToDo} style={{
-              backgroundColor:"blue",
-              width:100,
-              marginLeft:'auto',
-              marginRight:'auto',
-              alignItems:'center',
-              marginTop:20,
-              height:30,
-              borderRadius:10
-              
-              }}>
-              <Text style={{
-                color:'white',
-                marginTop:'auto',
-                marginBottom:'auto',
-                fontSize:16,
-                fontWeight:'800'
-                }}>Add Todo</Text>
+            <TouchableOpacity onPress={addToDo} style={styles.buttonStyle}>
+              <Text style={styles.buttonText}>Add Todo</Text>
             </TouchableOpacity>
-
-            {/* <Button 
-            title="Add TODO"
-            
-            /> */}
           </View>
 
           {todos.map(todo =>(
             <Text key={todo} style={{color:'red'}} >{todo}</Text>
           ))}
 
-
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
           
 
       </View>
@@ -77,5 +65,23 @@ const styles = StyleSheet.create({
     width:200,
     alignItems:'center'
     
+  },
+  buttonStyle:{
+    backgroundColor:"blue",
+    width:100,
+    marginLeft:'auto',
+    marginRight:'auto',
+    alignItems:'center',
+    marginTop:20,
+    height:30,
+    borderRadius:10
+  },
+  buttonText:{
+    color:'white',
+    marginTop:'auto',
+    marginBottom:'auto',
+    fontSize:16,
+    fontWeight:'800'
+
   }
 });
