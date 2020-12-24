2083b023809ebc93aec5e6c03091796b18d4cb9c
shehan82 <shehansandeepa82@gmail.com>
2020-12-24 21:05:47 +0530

    create ListEditScreen

---
+++ app/screens/ListEditScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AppTextInput from "../components/AppTextInput";
+
+const ListEditScreen = () => {
+  return (
+    <View>
+      <AppTextInput placeholder="Title" />
+    </View>
+  );
+};
+
+export default ListEditScreen;
+
+const styles = StyleSheet.create({});
