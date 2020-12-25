4602d6fc20e97d146041073f2eb1ded1e8bffd33
shehan82 <shehansandeepa82@gmail.com>
2020-12-25 23:09:00 +0530

    get images from library using expo image picker

--- app/components/PicImages.js
+++ app/components/PicImages.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, View, Image } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import * as Permission from "expo-permissions";
 
 const PicImages = () => {
+  const [url, setUrl] = useState();
+
   const getImagePermission = async () => {
     const permisson = await ImagePicker.requestCameraPermissionsAsync();
     const result = permisson.granted;
@@ -15,9 +17,23 @@ const PicImages = () => {
   useEffect(() => {
     getImagePermission();
   }, []);
+
+  const openLibrary = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      const url = result.uri;
+      setUrl(url);
+
+      console.log(url);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View>
-      <Text>helloooo</Text>
+      <Button title="press me if you can" onPress={openLibrary} />
+      <Image source={{ uri: url }} style={{ width: 100, height: 100 }} />
     </View>
   );
 };
