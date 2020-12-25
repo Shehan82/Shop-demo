0037a718e82d467720edc1525dea309b52e7e419
shehan82 <shehansandeepa82@gmail.com>
2020-12-26 00:30:20 +0530

    create a custom modal

--- app/components/PicImages.js
+++ app/components/PicImages.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Button, StyleSheet, Text, View, Image } from "react-native";
+import { Button, StyleSheet, Text, View, Image, Modal } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import * as Permission from "expo-permissions";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
 const PicImages = () => {
   const [url, setUrl] = useState();
+  const [modalVisibility, setModalVisibility] = useState(false);
 
   const getImagePermission = async () => {
     const permisson = await ImagePicker.requestCameraPermissionsAsync();
@@ -33,11 +35,47 @@ const PicImages = () => {
   return (
     <View>
       <Button title="press me if you can" onPress={openLibrary} />
-      <Image source={{ uri: url }} style={{ width: 100, height: 100 }} />
+      <TouchableWithoutFeedback
+        onPress={() => {
+          setModalVisibility(true);
+        }}
+      >
+        <Image source={{ uri: url }} style={{ width: 100, height: 100 }} />
+      </TouchableWithoutFeedback>
+
+      <Modal transparent={true} visible={modalVisibility} animationType="slide">
+        <View style={styles.outerModalView}>
+          <View style={styles.innerModalView}>
+            <Text>skldfj</Text>
+            <Button
+              title="close"
+              onPress={() => {
+                setModalVisibility(false);
+              }}
+            />
+          </View>
+        </View>
+      </Modal>
+      {/* </View> */}
     </View>
   );
 };
 
 export default PicImages;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  outerModalView: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    // borderColor: "red",
+    // borderWidth: 10,
+  },
+  innerModalView: {
+    width: 300,
+    height: 300,
+    borderColor: "blue",
+    borderWidth: 2,
+  },
+});
