d6adc8019f57c53b274eb78e148ea9a4db63fb83
shehan82 <shehansandeepa82@gmail.com>
2020-12-29 11:24:10 +0530

    add icon to the feed tab

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -5,12 +5,21 @@ import ListEditScreen from "../screens/ListEditScreen";
 import ListingScreen from "../screens/ListingScreen";
 import AccountNavigator from "./AccountNavigator";
 import FeedNavigator from "./FeedNavigator";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
   <Tab.Navigator>
-    <Tab.Screen name="Feed" component={FeedNavigator} />
+    <Tab.Screen
+      name="Feed"
+      component={FeedNavigator}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="home" color={color} size={size} />
+        ),
+      }}
+    />
     <Tab.Screen name="ListingEdit" component={ListEditScreen} />
     <Tab.Screen name="Account" component={AccountNavigator} />
   </Tab.Navigator>
