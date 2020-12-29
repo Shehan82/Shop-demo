04a0c05c81972f353b9ecd4b7f500efb651f9c26
shehan82 <shehansandeepa82@gmail.com>
2020-12-29 12:09:33 +0530

    hide headers in AccountNavigator and FeedNavigator

--- app/navigation/FeedNavigator.js
+++ app/navigation/FeedNavigator.js
@@ -6,7 +6,7 @@ import ListingScreen from "../screens/ListingScreen";
 const Stack = createStackNavigator();
 
 const FeedNavigator = () => (
-  <Stack.Navigator mode="modal">
+  <Stack.Navigator mode="modal" screenOptions={{ headerShown: false }}>
     <Stack.Screen name="Listing" component={ListingScreen} />
     <Stack.Screen name="ListingDetails" component={ListingDetailsScreen} />
   </Stack.Navigator>
